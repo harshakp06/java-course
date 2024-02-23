04dc8a8756f67e22b83c65e7d4a17600ead72519
Harsha Vardhan <kpharsha@yahoo.com>
2024-02-23 22:51:52 +0530

    add

---
+++ 01start/hello.java
@@ -0,0 +1,19 @@
+class Hello
+{
+    public static void main(String a[]) {
+       {
+        int num = 3;
+
+        int num1 = 5;
+        int num2 = 6;
+        int result = num1+num2;
+
+        System.out.println("Hello World");
+
+        System.out.println(num);
+
+        System.out.println(result);
+
+       } 
+    }
+}
