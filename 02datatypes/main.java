c86bf6850bd0035120a4b71e466891e7280b4861
Harsha Vardhan <kpharsha@yahoo.com>
2024-02-23 23:18:32 +0530

    add

--- 02datatypes/main.java
+++ 02datatypes/main.java
@@ -2,7 +2,47 @@ class main
 {
     public static void main(String a[]) {
         {
-            float marks = 
+            float marks = 6.5f;
+            double m = 7.88;
+
+            byte by = 127;
+            short sh = 558;
+            long l = 58856l;
+
+            char c = 'k';
+
+            boolean b = true;
+
+            System.out.println(marks);
+            System.out.println(m);
+            System.out.println(by);
+            System.out.println(sh);
+            System.out.println(l);
+            System.out.println(c);
+            System.out.println(b);
+
+
+
+
+
+
         }
     }
 }
+
+/*
+ * int 4 bytes
+ * long int - long - 8 bytes
+ * short int - short - 2 bytes
+ * byte - 1 byte - -128 to 128 - 2^7
+ */
+
+ /*
+  * float num - 4 bytes
+  * double float - 8 bytes
+  */
+
+  /*
+   * char 2 bytes
+   * boolean - true or flase - 0,1
+   */
